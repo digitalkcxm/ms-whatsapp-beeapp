a840ba9b4b698eefc6085ca34bf5d2f4704a37eb
Silas S <silas.silva@digitalk.com.br>
2024-03-21 15:07:01 -0300

    fix: correção de migration

--- src/api/config/database/migrations/20240320153300_alter_instances.js
+++ src/api/config/database/migrations/20240320153300_alter_instances.js
@@ -1,5 +1,5 @@
-const up = (knex) => knex.raw(`ALTER TABLE instances ADD COLUMN id_config INTEGER NOT NULL REFERENCES configs(id)`)
+const up = (knex) => knex.raw(`ALTER TABLE instances ADD COLUMN IF NOT EXISTS id_config INTEGER REFERENCES configs(id)`)
 
-const down = (knex) => knex.raw(`ALTER TABLE instances DROP COLUMN id_config`)
+const down = (knex) => knex.raw(`ALTER TABLE instances DROP COLUMN IF EXISTS id_config`)
 
 export { up, down }
